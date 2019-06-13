a2c21fde9ae1874e35ad3cbdf18c18424f7978fa
Kevin <Kevin@Kevin-PC.speedport.ip>
2019-06-13 19:14:49 +0200

    Funzt alles

--- src/MP4/MultiHashMap.java
+++ src/MP4/MultiHashMap.java
@@ -32,20 +32,21 @@ public class MultiHashMap<K, V> implements MultiMap<K, V> {
 	@Override
 	public boolean containsKey(K key) throws NullPointerException {
 		// TODO Auto-generated method stub
-		if (key == null)
+		if (key == null) {
 			throw new NullPointerException("Key cannot be null");
-		else
+		} else {
 			return getKeys().contains(key);
+		}
 	}
 
 	@Override
 	public List<V> removeKey(K key) throws KeyNotFoundException, NullPointerException {
 		// TODO Auto-generated method stub
-		if (key == null)
-			throw new KeyNotFoundException("key cannot be null");
-		else if (!containsKey(key))
-			throw new NullPointerException("No key found");
-		else {
+		if (key == null) {
+			throw new NullPointerException("Key cannot be null");
+		} else if (!containsKey(key)) {
+			throw new KeyNotFoundException("Key not found");
+		} else {
 			List<V> list = m.get(key);
 			m.remove(key);
 			return list;
@@ -56,76 +57,73 @@ public class MultiHashMap<K, V> implements MultiMap<K, V> {
 	@Override
 	public List<V> getValues(K key) throws KeyNotFoundException, NullPointerException {
 		// TODO Auto-generated method stub
-		if (key == null)
-			throw new KeyNotFoundException("Key cannot be null");
-		else if (!containsKey(key))
-			throw new NullPointerException("No key found");
-		else
+		if (key == null) {
+			throw new NullPointerException("Key cannot be null");
+		} else if (!containsKey(key)) {
+			throw new KeyNotFoundException("Key not found");
+		} else {
 			return m.get(key);
-	}
+		}
 
-	public List<V> getKey(K key) throws KeyNotFoundException, NullPointerException {
-		return m.get(key);
 	}
 
 	@Override
 	public boolean removeValue(K key, V value) throws KeyNotFoundException, NullPointerException {
 		// TODO Auto-generated method stub
-		if (key == null)
-			throw new KeyNotFoundException("key cannot be null");
-		else if (value == null)
-			throw new NullPointerException("value cannot be null");
-		else if (!containsKey(key))
-			throw new NullPointerException("no key found");
-		else {
+		if (key == null) {
+			throw new KeyNotFoundException("Key cannot be null");
+		} else if (value == null) {
+			throw new NullPointerException("Value cannot be null");
+		} else if (!containsKey(key)) {
+			throw new NullPointerException("Key not found");
+		} else {
 			if (m.get(key).contains(value)) {
+				System.out.println("Es wird " + m.get(key) + " aus " + "[" + key + "]" + " entfernt");
+//				System.out.println(m.get(key).size());
 				m.get(key).remove(value);
-				return true;
-			} else if (m.get(key) == null) {
-				m.remove(key);
+//				System.out.println(m.get(key).size());
+				if (m.get(key).size() == 0) {
+					m.remove(key);
+				}
 
-				return true;
-			} else
-				return false;
+			}
+
+			return false;
 		}
-
 	}
 
 	private void insertNewKeyPair(K key, V value) {
-		List<V> list = new ArrayList<>(Arrays.asList(value));
+		List<V> list = new ArrayList<>();
+		list.add(value);
 		m.put(key, list);
-	}
 
-	private void insertNewValueForOldKey(K key, V value) {
-		m.get(key).add(value);
 	}
 
 	@Override
 	public void addValue(K key, V value) throws NullPointerException {
 		// TODO Auto-generated method stub
-		if (key == null)
-			throw new NullPointerException("key cannot be null");
-		else if (value == null)
+		if (key == null) {
+			throw new NullPointerException("Key cannot be null");
+		} else if (value == null) {
 			throw new NullPointerException("Value cannot be null");
-		else if (!containsKey(key)) {
-			insertNewKeyPair(key, value);
-		} else
-			insertNewValueForOldKey(key, value);
-
-	} // TODO: change hierarchy
-
-	@Override
-	public String toString() {
-
-		StringBuilder builder = new StringBuilder();
-
-		for (K key : getKeys()) {
-			for (V value : m.get(key)) {
-				builder.append(key.toString()).append(" = ").append(value.toString()).append(System.lineSeparator());
+		} else {
+			if (!containsKey(key)) {
+				insertNewKeyPair(key, value);
+			} else {
+				m.get(key).add(value);
 			}
 		}
 
-		return builder.toString();
+	}
+
+	@Override
+	public String toString() {
+		String ausgabe = "";
+
+		for (K key : getKeys()) {
+			System.out.println(key + "\t" + " = " + m.get(key));
+		}
+		return ausgabe;
 	}
 
 	public static void main(String[] args) throws NullPointerException, KeyNotFoundException {
@@ -153,29 +151,39 @@ public class MultiHashMap<K, V> implements MultiMap<K, V> {
 		System.out.println("----Ausgangsliste:----");
 		System.out.println(multiHashMap0.toString() + System.lineSeparator());
 
-		System.out.println("----Nach Bearbeitung (Remove Value):----");
-		multiHashMap0.removeValue("one", "valueOne0");
-		multiHashMap0.removeValue("one", "valueOne1");
-		multiHashMap0.removeValue("one", "valueOne2");
-
-		multiHashMap0.removeValue("two", "valueOne1");
+		System.out.println("----Nach Bearbeitung (Remove Value)----");
+		// multiHashMap0.removeValue("one", "valueOne0");
+		// multiHashMap0.removeValue("one", "valueOne1");
+		// multiHashMap0.removeValue("one", "valueOne2");
+		multiHashMap0.removeValue("five", "valueFive0");
+		multiHashMap0.removeValue("five", "valueFive1");
 		System.out.println(multiHashMap0.toString() + System.lineSeparator());
 
-		System.out.println("----Nach Bearbeitung (Remove key):----");
+		System.out.println("----Nach Bearbeitung (Remove key three)----");
 		multiHashMap0.removeKey("three");
 		System.out.println(multiHashMap0.toString() + System.lineSeparator());
-
-		// multiHash0.addValue("Tiername", "Daisy");
-		// multiHash0.addValue("Tiername", "Lucy");
-		// multiHash0.addValue("Tiername", "HundeHund");
-		//
-		// multiHash0.addValue("Menschenname", "Kevin");
-		// multiHash0.addValue("Menschenname", "Sarah");
-		// multiHash0.addValue("Menschenname", "Rudi");
-		//
-		// multiHash0.addValue("Nachname", "Ludi");
-		// multiHash0.addValue("Nachname", "Gudi");
-		// multiHash0.addValue("Nachname", "Hudi");
+		
+		System.out.println("---- Ausgangsliste----");
+		MultiHashMap<Integer, String> multiHashMap1 = new MultiHashMap<>();
+		
+		multiHashMap1.addValue(1, "eins0");
+		multiHashMap1.addValue(1, "eins1");
+		multiHashMap1.addValue(1, "eins2");
+		multiHashMap1.addValue(1, "eins3");
+		
+		multiHashMap1.addValue(2, "zwei0");
+		multiHashMap1.addValue(2, "zwei1");
+		multiHashMap1.addValue(2, "zwei2");
+		multiHashMap1.addValue(2, "zwei3");
+		
+		multiHashMap1.addValue(3, "drei0");
+		
+		System.out.println(multiHashMap1.toString());	
+		
+		multiHashMap1.removeValue(3, "drei0");
+		
+		System.out.println(multiHashMap1.toString());
+		
 
 	}
 
